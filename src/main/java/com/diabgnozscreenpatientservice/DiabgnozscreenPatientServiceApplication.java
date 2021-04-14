e94a820add189ae0e578db1c0b672847f0851cea
Loïc Stringer <loic.stringer@gmail.com>
2021-04-14 22:27:50 +0200

    Initial commit

---
+++ src/main/java/com/diabgnozscreenpatientservice/DiabgnozscreenPatientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.diabgnozscreenpatientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabgnozscreenPatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabgnozscreenPatientServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diabgnozscreenpatientservice/DiabgnozscreenPatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diabgnozscreenpatientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabgnozscreenPatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
