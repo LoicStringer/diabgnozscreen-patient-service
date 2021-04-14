c88940e578f18b2097d3d078a764be2aca3f5273
Loïc Stringer <loic.stringer@gmail.com>
2021-04-15 00:42:43 +0200

    Fetch patient by name or birthdate methods

---
+++ src/main/java/com/diabgnozscreenpatientservice/exception/PatientNotFoundException.java
@@ -0,0 +1,13 @@
+package com.diabgnozscreenpatientservice.exception;
+
+public class PatientNotFoundException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public PatientNotFoundException(String message) {
+		super(message);
+	}
+
+	
+
+}
