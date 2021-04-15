423746c32992b477062dd53b800b506cf479a6df
Loïc Stringer <loic.stringer@gmail.com>
2021-04-15 14:55:24 +0200

    Exceptions handling mechanism

---
+++ src/main/java/com/diabgnozscreenpatientservice/exception/ExceptionsHandlerCenter.java
@@ -0,0 +1,46 @@
+package com.diabgnozscreenpatientservice.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class ExceptionsHandlerCenter  extends ResponseEntityExceptionHandler{
+
+	@ExceptionHandler(PatientNotFoundException.class)
+	public ResponseEntity<ExceptionResponse> handleNotAllowedIdSettingException(PatientNotFoundException ex) {
+		ExceptionResponse exceptionResponse = exceptionResponseBuild(ex);
+		return new ResponseEntity<ExceptionResponse>(exceptionResponse, getHttpStatusFromException(ex));
+	}
+	
+	private ExceptionResponse exceptionResponseBuild(Exception ex) {
+		String statusCode = getStatusCodeFromException(ex);
+		ExceptionResponse exceptionResponse = new ExceptionResponse(LocalDateTime.now(), statusCode,
+				ex.getClass().getSimpleName(), ex.getMessage());
+		return exceptionResponse;
+	}
+
+	private HttpStatus getHttpStatusFromException(Exception ex) {
+		ResponseStatus responseStatus = AnnotationUtils.findAnnotation(ex.getClass(), ResponseStatus.class);
+		HttpStatus status = responseStatus.value();
+		return status;
+	}
+
+	private String getStatusCodeFromException(Exception ex) {
+		ResponseStatus responseStatus = AnnotationUtils.findAnnotation(ex.getClass(), ResponseStatus.class);
+		HttpStatus status = responseStatus.code();
+		return status.toString();
+	}
+	
+	private String getReasonFromResponseStatus(Exception ex) {
+		ResponseStatus responseStatus = AnnotationUtils.findAnnotation(ex.getClass(), ResponseStatus.class);
+		return responseStatus.reason();
+	}
+	
+}
--- src/main/java/com/diabgnozscreenpatientservice/exception/PatientNotFoundException.java
+++ src/main/java/com/diabgnozscreenpatientservice/exception/PatientNotFoundException.java
@@ -1,8 +1,20 @@
 package com.diabgnozscreenpatientservice.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value=HttpStatus.NOT_FOUND,reason="This patient is not registered.")
 public class PatientNotFoundException extends Exception{
 
 	private static final long serialVersionUID = 1L;
+
+	
+	
+	public PatientNotFoundException() {
+		super();
+	}
+
+
 
 	public PatientNotFoundException(String message) {
 		super(message);
